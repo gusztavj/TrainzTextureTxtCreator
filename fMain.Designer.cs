46eb6b2bd5ed4ce824f3ffaa528836e90b8c1705
t1nk-r <janvari.gusztav@imprestige.biz>
2021-05-02 18:47:58 +0200

    Add project files.

---
+++ fMain.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace TrainzTextureTxtCreator
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.cmdSelectFolder = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eFolderPath = new System.Windows.Forms.TextBox();
+            this.cmdGO = new System.Windows.Forms.Button();
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // cmdSelectFolder
+            // 
+            this.cmdSelectFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdSelectFolder.Location = new System.Drawing.Point(627, 47);
+            this.cmdSelectFolder.Name = "cmdSelectFolder";
+            this.cmdSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.cmdSelectFolder.TabIndex = 0;
+            this.cmdSelectFolder.Text = "&Select folder";
+            this.cmdSelectFolder.UseVisualStyleBackColor = true;
+            this.cmdSelectFolder.Click += new System.EventHandler(this.cmdSelectFolder_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 52);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(103, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Folder of image files:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select the folder of the texture image files and hit Go";
+            // 
+            // eFolderPath
+            // 
+            this.eFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.eFolderPath.Location = new System.Drawing.Point(179, 49);
+            this.eFolderPath.Name = "eFolderPath";
+            this.eFolderPath.Size = new System.Drawing.Size(442, 20);
+            this.eFolderPath.TabIndex = 3;
+            // 
+            // cmdGO
+            // 
+            this.cmdGO.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdGO.Location = new System.Drawing.Point(15, 93);
+            this.cmdGO.Name = "cmdGO";
+            this.cmdGO.Size = new System.Drawing.Size(687, 23);
+            this.cmdGO.TabIndex = 4;
+            this.cmdGO.Text = "GO";
+            this.cmdGO.UseVisualStyleBackColor = true;
+            this.cmdGO.Click += new System.EventHandler(this.cmdGO_Click);
+            // 
+            // lbLog
+            // 
+            this.lbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(15, 133);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(687, 251);
+            this.lbLog.TabIndex = 5;
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdExit.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.cmdExit.Location = new System.Drawing.Point(12, 400);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(687, 23);
+            this.cmdExit.TabIndex = 6;
+            this.cmdExit.Text = "Bye";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 433);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.cmdGO);
+            this.Controls.Add(this.eFolderPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.cmdSelectFolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fMain";
+            this.Text = "Trainz Texture TXT Creator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fMain_FormClosing);
+            this.Load += new System.EventHandler(this.fMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSelectFolder;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox eFolderPath;
+        private System.Windows.Forms.Button cmdGO;
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+    }
+}
+
---
+++ fMain.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TrainzTextureTxtCreator
+{
+    public partial class fMain : Form
+    {
+
+        private string ImagePath
+        {
+            get { return Properties.Settings.Default.FolderPath; }
+            set { Properties.Settings.Default.FolderPath = value; }
+            
+        }
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdSelectFolder_Click(object sender, EventArgs e)
+        {
+            fbd.SelectedPath = ImagePath;
+            if (fbd.ShowDialog() == DialogResult.OK) {
+                ImagePath = fbd.SelectedPath;
+            }
+        }
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveSettings()
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void fMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveSettings();
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            eFolderPath.Text = ImagePath;
+        }
+
+        private void cmdGO_Click(object sender, EventArgs e)
+        {
+            ImagePath = eFolderPath.Text;
+            List<string> files = Directory.GetFiles(Path.Combine(ImagePath), "*.png").ToList<string>();
+            //IEnumerable<string> normals = files.Where(file => file.Contains("normal.png"));
+
+            lbLog.Items.Add($"Started processing {ImagePath}...");
+            int itemIx = 0;
+
+            foreach (string file in files)
+            {
+                string pureFileName = Path.GetFileNameWithoutExtension(file);
+                string contents = "";
+
+                if (file.Contains("albedo.png"))
+                {
+                    contents += $"Primary={pureFileName}.png\n";
+                    contents += $"Tile=st";
+                    lbLog.Items.Add($"{++itemIx}. Created albedo descriptor for {Path.GetFileName(file)}");
+                }
+
+                if (file.Contains("normal.png"))
+                {
+                    contents += $"Primary={pureFileName}.png\n";
+                    contents += $"Alpha={pureFileName}.png\n";
+                    contents += $"NormalMapHint=normalmap\n";
+                    contents += $"Tile=st";
+                    lbLog.Items.Add($"{++itemIx}. Created normal descriptor for {Path.GetFileName(file)}");
+                }
+
+                if (file.Contains("parameter.png") || file.Contains("parameters.png"))
+                {
+                    contents += $"Primary={pureFileName}.png\n";
+                    contents += $"Alpha={pureFileName}.png\n";
+                    contents += $"Tile=st";
+                    lbLog.Items.Add($"{++itemIx}. Created parameter descriptor for {Path.GetFileName(file)}");
+                }
+
+
+                string textureFile = Path.Combine(ImagePath, pureFileName + ".texture.txt");
+                File.WriteAllText(textureFile, contents);
+                lbLog.Items.Add($"\tFile {textureFile} written successfully");
+            }
+            lbLog.Items.Add($"Done");
+
+        }
+    }
+}
